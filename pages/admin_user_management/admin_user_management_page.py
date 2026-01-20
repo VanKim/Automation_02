b12e6e4b09d1582e34787fe736c8b2999bf14779
hoangtranthuvan <hoangtranthuvan@gmail.com>
2026-01-20 19:42:43 +0700

    ci/cd

--- pages/admin_user_management/admin_user_management_page.py
+++ pages/admin_user_management/admin_user_management_page.py
@@ -1,8 +1,9 @@
-from pages.admin_user_management.admin_user_management_searching_form import AdminSearchingForm
-from pages.admin_user_management.admin_user_management_user_table import AdminUserTable
+from pages.admin_user_management.admin_user_management_searching_form import SearchingForm
+from pages.admin_user_management.admin_user_management_user_table import UserTable
 
 
-class AdminPage(AdminSearchingForm, AdminUserTable):
+class AdminPage(SearchingForm, UserTable):
 
     def __init__(self, driver):
         super().__init__(driver)
+        
--- tests/admin_page/test_admin.py
+++ tests/admin_page/test_admin.py
@@ -1,26 +1,25 @@
-from utils.config_reader import ConfigReader
-from pages.admin_user_management.admin_user_management_searching_form import SearchingForm
-from pages.admin_user_management.admin_user_management_user_table import UserTable
-from apis.hrm_user_api import HRMUserApi
+from pages.admin_user_management.admin_user_management_page import AdminPage
+import pytest
+#from apis.hrm_user_api import HRMUserApi
 
+@pytest.mark.smoke
 class TestAdminPage:
-    '''def test_select_custom_dropdown(self, admin_page_driver):
-        searching_form = SearchingForm(admin_page_driver)
-        searching_form.select_user_role_dropdown("Admin")
-        assert  searching_form.get_user_role_item() == "Admin"
-    '''
-    '''def test_redirect_adding_form(self, admin_page_driver):
-        user_table = UserTable(admin_page_driver)
-        user_table.click_on_add_button()
+    def test_select_custom_dropdown(self, admin_page_driver):
+        admin_page = AdminPage(admin_page_driver)
+        admin_page.select_user_role_dropdown("Admin")
+        assert admin_page.get_user_role_item() == "Admin"
+    
+    def test_redirect_adding_form(self, admin_page_driver):
+        admin_page = AdminPage(admin_page_driver)
+        admin_page.click_on_add_button()
         assert admin_page_driver.current_url == 'https://opensource-demo.orangehrmlive.com/web/index.php/admin/saveSystemUser'
-    '''
+    
     def test_searching_with_user_role(self, admin_page_driver):
-        searching_form = SearchingForm(admin_page_driver)
-        searching_form.select_user_role_dropdown("Admin")
-        searching_form.click_on_search_button()
-        user_table = UserTable(admin_page_driver)
+        admin_page = AdminPage(admin_page_driver)
+        admin_page.select_user_role_dropdown("Admin")
+        admin_page.click_on_search_button()
         #response = HRMUserApi.get_all_users(admin_page_driver)
         #print(response.url, response.headers)
         #assert response.status_code == 200
-        #assert  user_table.get_total_number_user() == 10
+        assert  admin_page.get_total_number_user() == 10
 
