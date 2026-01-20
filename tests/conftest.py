82d5692f0dd8e330b9b64495ea3e8456fdd6be8e
hoangtranthuvan <hoangtranthuvan@gmail.com>
2026-01-20 20:45:01 +0700

    ci/cd lan 13

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,9 +21,6 @@ def credentials():
 def driver():
     options = Options()
     options.add_argument("--headless=new")
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
     # open browser
     driver = webdriver.Chrome(options=options)
     base_url = ConfigReader.get_base_url()
