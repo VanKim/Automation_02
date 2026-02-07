f7191d1aa89b3a81b9a8e13626fa0f6e86a9d020
hoangtranthuvan <hoangtranthuvan@gmail.com>
2026-02-07 11:09:15 +0700

    test ci lan 8

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,10 @@
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
 
-class LoginPage:
+class LoginPage(BasePage):
     def __init__(self, driver):
+        super().__init__(driver)
         self.driver = driver
         self.__username__ = (By.NAME, "username") #tuple (str, str)
         self.__password__ = (By.NAME, "password")
@@ -10,34 +12,24 @@ class LoginPage:
         self.__error_message__ = (By.CSS_SELECTOR, ".oxd-alert--error")
 
     def input_username_field(self, username):
-        self.driver.find_element(*self.__username__).send_keys(username) #*self.__userName__ unpack tuple to 2 args
+        self.send_keys(self.__username__,username) #*self.__userName__ unpack tuple to 2 args
 
     def input_password_field(self, password):
-        self.driver.find_element(*self.__password__).send_keys(password)
-
-    def clear_username_field(self):
-        self.driver.find_element(*self.__username__).clear()
-
-    def clear_password_field(self):
-        self.driver.find_element(*self.__password__).clear()
+        self.send_keys(self.__password__,password)
 
     def click_login_button(self):
-        self.driver.find_element(*self.__button__).click()
+        self.click(self.__button__)
 
     def press_enter_login_button(self):
-        self.driver.find_element(*self.__button__).send_keys(Keys.ENTER)
+        self.send_keys(self.__button__,Keys.ENTER)
 
     def login_without_keyBoard(self, username, password):
-        self.clear_username_field()
         self.input_username_field(username)
-        self.clear_password_field()
         self.input_password_field(password)
         self.click_login_button()
 
     def login_with_enter_keyBoard(self, username, password):
-        self.clear_username_field()
         self.input_username_field(username)
-        self.clear_password_field()
         self.input_password_field(password)
         self.press_enter_login_button()
 
