b6e30641e13b5f8510177a122f0dcfc857483725
hoangtranthuvan <hoangtranthuvan@gmail.com>
2026-03-04 09:30:48 +0700

    tests/apis/test_update_user_account.py

--- tests/apis/test_update_user_account.py
+++ tests/apis/test_update_user_account.py
@@ -27,7 +27,7 @@ class TestUpdateUserAccountApi:
         validate(instance=json.loads(response.request.body.decode('utf-8')), schema=request_schema)
         validate(instance=response.json(), schema=response_schema)
 
-    def test_update_user_account_successfully(self):
+    def test_update_user_account_success(self):
         user_id = 54
         payload={
                 "changePassword": False,
@@ -40,13 +40,33 @@ class TestUpdateUserAccountApi:
         response = UserAccountApis.update_user_account(payload, user_id)
         assert response.status_code == 200
         response = response.json()
+        check.equal(response["data"]["changePassword"], False)
         # Verify comparing response with payload
         check.equal(response["data"]["id"], user_id)
         check.equal(response["data"]["userName"], payload["username"])
         check.equal(response["data"]["status"], payload["status"])
-        check.equal(response["data"]["changePassword"], payload["changePassword"])
         check.equal(response["data"]["empNumber"], payload["empNumber"])
-        check.equal(response["data"]["password"], payload["password"])
+        check.equal(response["data"]["userRole"]["userRoleId"], payload["id"])
+
+    def test_update_user_account_change_password_success(self):
+        user_id = 54
+        payload={
+                "changePassword": True,
+                "empNumber": 137,
+                "password": "admin123",
+                "status": True,
+                "userRoleId": 2,
+                "username": "FMLName EDIT1"
+        }
+        response = UserAccountApis.update_user_account(payload, user_id)
+        assert response.status_code == 200
+        response = response.json()
+        print(response)
+        # Verify comparing response with payload
+        check.equal(response["data"]["id"], user_id)
+        check.equal(response["data"]["userName"], payload["username"])
+        check.equal(response["data"]["status"], payload["status"])
+        check.equal(response["data"]["employee"]["empNumber"], payload["empNumber"])
         check.equal(response["data"]["userRole"]["userRoleId"], payload["id"])
 
     def test_update_user_account_invalid_format_data(self):
