1385a856c3665736c07a2e04971f1b417ec8eb46
hoangtranthuvan <hoangtranthuvan@gmail.com>
2026-03-08 11:51:42 +0700

    tests/ui/functional/admin_page/conftest.py

--- tests/ui/functional/admin_page/conftest.py
+++ tests/ui/functional/admin_page/conftest.py
@@ -6,23 +6,24 @@ from selenium.webdriver.common.by import By
 
 @pytest.fixture(scope="function")
 def admin_page_driver(login_driver):
-    wait = WebDriverWait(login_driver, 15)
+    wait = WebDriverWait(login_driver, 40)
     print(f'[DEBUG]\n\nCURRENT URL{login_driver.current_url}\n\n')
-    try:
-        wait.until(
-            EC.presence_of_element_located(
-                (By.XPATH, "//span[normalize-space(.)='Admin']")
-            )
-        )
 
-        admin_menu = wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "//span[normalize-space(.)='Admin']")
-            )
+    wait.until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//span[normalize-space(.)='Admin']")
         )
-        admin_menu.click()
-        wait.until(EC.url_contains("/admin/viewSystemUsers"))
-        print("\n\n\n\nJFGKFBKGJBDKFBGKDB\n\n\n\n")
-    except TimeoutException:
-        login_driver.save_screenshot("report/debug_admin.png")
+    )
+
+    admin_menu = wait.until(
+        EC.element_to_be_clickable(
+            (By.XPATH, "//span[normalize-space(.)='Admin']")
+        )
+    )
+
+    admin_menu.click()
+    print("\n\n\n\n[INFO:] JFGKFBKGJBDKFBGKDB\n\n\n\n")
+    wait.until(EC.url_contains("/admin/viewSystemUsers"))
+    print("\n\n\n\n[INFO:] JFGKFBKGJBDKFBGKDB\n\n\n\n")
+
     return login_driver
