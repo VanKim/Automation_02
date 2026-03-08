d84f71b3293b4c35db06def4d8ea4d60552096c5
hoangtranthuvan <hoangtranthuvan@gmail.com>
2026-03-08 12:43:34 +0700

    tests/ui/functional/admin_page/conftest.py

--- tests/ui/functional/admin_page/conftest.py
+++ tests/ui/functional/admin_page/conftest.py
@@ -6,12 +6,12 @@ from selenium.webdriver.common.by import By
 
 @pytest.fixture(scope="function")
 def admin_page_driver(login_driver):
-    wait = WebDriverWait(login_driver, 50)
+    wait = WebDriverWait(login_driver, 100)
     print(f'[DEBUG]\n\nCURRENT URL{login_driver.current_url}\n\n')
-    locator = (By.XPATH, "//a[contains(@href, 'admin/viewAdminModule')]")
+    locator = (By.XPATH, "//nav[@role='navigation']//*[text()='Admin']")
     print(f'[DEBUG]\n\nPAGE SOURCE: {login_driver.page_source}\n\n')
     print("Admin exist:", login_driver.find_elements(*locator))
-    admin_page = wait.until(EC.element_to_be_clickable(locator))
+    admin_page = wait.until(EC.element_to_be_clickable(locator)).click()
     #print(f"\n\n\n\n[INFO2:] JFGKFBKGJBDKFBGKDB{admin_page}\n\n\n\n")
     #admin_page.click()
     wait.until(EC.url_contains("/admin/viewSystemUsers"))
