537dc4b014ae16d291ed97dc83eba2fe12af6c63
hoangtranthuvan <hoangtranthuvan@gmail.com>
2026-03-08 12:10:48 +0700

    tests/ui/functional/admin_page/conftest.py

--- tests/ui/functional/admin_page/conftest.py
+++ tests/ui/functional/admin_page/conftest.py
@@ -9,10 +9,13 @@ def admin_page_driver(login_driver):
     wait = WebDriverWait(login_driver, 40)
     print(f'[DEBUG]\n\nCURRENT URL{login_driver.current_url}\n\n')
     locator = (By.XPATH, "//a[contains(@href, 'admin/viewAdminModule')]")
-    admin_page = wait.until(
+    wait.until(
         EC.presence_of_element_located(locator)
     )
-    print(f"\n\n\n\n[INFO1:] JFGKFBKGJBDKFBGKDB {admin_page.get_attribute("outerHTML")}\n\n\n\n")
+    wait.until(
+        EC.visibility_of_element_located(locator)
+    )
+    print(f"\n\n\n\n[INFO1:] JFGKFBKGJBDKFBGKDB gghhhh\n\n\n\n")
     admin_page = wait.until(
         EC.element_to_be_clickable(locator)
     ).click()
