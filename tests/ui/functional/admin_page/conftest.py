25262050c10d440c641a8fbd090174a652d979e0
hoangtranthuvan <hoangtranthuvan@gmail.com>
2026-03-08 12:05:48 +0700

    tests/ui/functional/admin_page/conftest.py

--- tests/ui/functional/admin_page/conftest.py
+++ tests/ui/functional/admin_page/conftest.py
@@ -13,13 +13,6 @@ def admin_page_driver(login_driver):
         EC.presence_of_element_located(locator)
     )
     admin_page.click()
-    print(f"\n\n\n\n[INFO1:] JFGKFBKGJBDKFBGKDB {admin_page.get_attribute("outerHTML")}\n\n\n\n")
-    admin_page = wait.until(
-        EC.element_to_be_clickable(
-            (By.XPATH, "//a[contains(@href, 'admin/viewAdminModule')]")
-        )
-    )
-    admin_page.click()
     print("\n\n\n\n[INFO2:] JFGKFBKGJBDKFBGKDB\n\n\n\n")
     wait.until(EC.url_contains("/admin/viewSystemUsers"))
     print("\n\n\n\n[INFO3:] JFGKFBKGJBDKFBGKDB\n\n\n\n")
