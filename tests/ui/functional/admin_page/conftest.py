92d68b28d5c59082df923d98c412f672f7d1bdaf
hoangtranthuvan <hoangtranthuvan@gmail.com>
2026-03-08 12:02:35 +0700

    tests/ui/functional/admin_page/conftest.py

--- tests/ui/functional/admin_page/conftest.py
+++ tests/ui/functional/admin_page/conftest.py
@@ -8,22 +8,19 @@ from selenium.webdriver.common.by import By
 def admin_page_driver(login_driver):
     wait = WebDriverWait(login_driver, 40)
     print(f'[DEBUG]\n\nCURRENT URL{login_driver.current_url}\n\n')
-
-    wait.until(
-        EC.presence_of_element_located(
-            (By.XPATH, "//a[contains(@href, 'admin/viewAdminModule')]")
-        )
+    locator = (By.XPATH, "//a[contains(@href, 'admin/viewAdminModule')]")
+    admin_page = wait.until(
+        EC.presence_of_element_located(locator)
     )
-
-    admin_menu = wait.until(
+    print(f"\n\n\n\n[INFO1:] JFGKFBKGJBDKFBGKDB {admin_page.get_attribute("outerHTML")}\n\n\n\n")
+    admin_page = wait.until(
         EC.element_to_be_clickable(
             (By.XPATH, "//a[contains(@href, 'admin/viewAdminModule')]")
         )
     )
-
-    admin_menu.click()
-    print("\n\n\n\n[INFO:] JFGKFBKGJBDKFBGKDB\n\n\n\n")
+    admin_page.click()
+    print("\n\n\n\n[INFO2:] JFGKFBKGJBDKFBGKDB\n\n\n\n")
     wait.until(EC.url_contains("/admin/viewSystemUsers"))
-    print("\n\n\n\n[INFO:] JFGKFBKGJBDKFBGKDB\n\n\n\n")
+    print("\n\n\n\n[INFO3:] JFGKFBKGJBDKFBGKDB\n\n\n\n")
 
     return login_driver
