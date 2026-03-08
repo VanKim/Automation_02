f2e4603f231c0c6fb3f6b835e6c76a1c4817d107
hoangtranthuvan <hoangtranthuvan@gmail.com>
2026-03-08 13:27:57 +0700

    tests/ui/functional/admin_page/conftest.py

--- tests/ui/functional/admin_page/conftest.py
+++ tests/ui/functional/admin_page/conftest.py
@@ -6,14 +6,15 @@ from selenium.webdriver.common.by import By
 
 @pytest.fixture(scope="function")
 def admin_page_driver(login_driver):
-    wait = WebDriverWait(login_driver, 200)
+    wait = WebDriverWait(login_driver, 250)
     print(f'[DEBUG]\n\nCURRENT URL{login_driver.current_url}\n\n')
     locator = (By.XPATH, "//nav[@role='navigation']//*[text()='Admin']/ancestor::a")
     admin_page = wait.until(EC.presence_of_element_located(locator))
     #print(f'[DEBUG]\n\nPAGE SOURCE: {login_driver.page_source}\n\n')
-    print(f'[DEBUG]\n\nPAURCE: {admin_page.get_attribute('outerHTML')}\n\n')
+    print(f'[DEBUG]\n\nPAURCE1111: {admin_page.get_attribute('outerHTML')}\n\n')
     #admin_page = wait.until(EC.visibility_of_element_located(locator))
-    #admin_page = wait.until(EC.element_to_be_clickable(locator))
+    admin_page = wait.until(EC.element_to_be_clickable(locator))
+    print(f'[DEBUG]\n\nPAURCE2222: {admin_page}\n\n')
     #admin_page.click()
     #wait.until(EC.url_contains("/admin/viewSystemUsers"))
     print(f'[DEBUG]\n\nCURRENT URL{login_driver.current_url}\n\n')
