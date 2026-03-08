f6456e78e88b445710e06983a22881e502d6e74c
hoangtranthuvan <hoangtranthuvan@gmail.com>
2026-03-08 09:00:14 +0700

    tests/ui/functional/admin_page/conftest.py

--- tests/ui/functional/admin_page/conftest.py
+++ tests/ui/functional/admin_page/conftest.py
@@ -6,22 +6,21 @@ from selenium.webdriver.common.by import By
 @pytest.fixture(scope="function")
 def admin_page_driver(login_driver):
     wait = WebDriverWait(login_driver, 30)
+
     # wait dashboard load
     print(f'\n\nCURRENT URL{login_driver.current_url}\n\n')
+    wait.until(
+        EC.presence_of_element_located(
+            (By.CLASS_NAME, "oxd-sidepanel")
+        )
+    )
+
     wait.until(
         EC.presence_of_element_located(
             (By.XPATH, "//span[normalize-space(.)='Admin']")
         )
     )
 
-    # wait.until(EC.element_to_be_clickable(
-    #     (By.XPATH, "//span[normalize-space(.)='Admin']")
-    # )).click()
-    wait.until(
-        EC.presence_of_element_located(
-            (By.CLASS_NAME, "oxd-sidepanel")
-        )
-    )
     admin_menu = wait.until(
         EC.element_to_be_clickable(
             (By.XPATH, "//span[normalize-space(.)='Admin']")
